d119981b933410f0cd0de8d19e011b24c268cef2
wangtingshu <ting.shu@mail.utoronto.ca>
2019-10-19 15:17:21 -0400

    Added some error handling and checking valid emails.

--- app/src/main/java/com/example/userinterface/SignupActivity.java
+++ app/src/main/java/com/example/userinterface/SignupActivity.java
@@ -6,12 +6,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText etName, etEmail, etPassword;
-    String name, email, password;
+    EditText etName, etEmail, etPassword, etRePassword;
+    String name, email, password, rePassword;
     Button btnRegister;
 
     @Override
@@ -22,6 +25,7 @@ public class SignupActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.email);
         etPassword = findViewById(R.id.password);
         btnRegister = findViewById(R.id.register);
+        etRePassword = findViewById(R.id.rePassword);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,13 +33,38 @@ public class SignupActivity extends AppCompatActivity {
                 name = etName.getText().toString();
                 email = etEmail.getText().toString();
                 password = etPassword.getText().toString();
-                String task = "register";
-                BackgroundActivity backgroundTask = new BackgroundActivity(SignupActivity.this);
-                backgroundTask.execute(task, name, email, password);
-                finish();
+                rePassword = etRePassword.getText().toString();
+                //Check if the email is in a valid format and the password is entered the same.
+                if (!password.equals(rePassword) || !isValid(email)){
+                    errorMessage("Incorrect Info", "Re-enter information.");
+                }else{
+                    String task = "register";
+                    BackgroundActivity backgroundTask = new BackgroundActivity(SignupActivity.this);
+                    backgroundTask.execute(task, name, email, password);
+                    finish();
+                }
+
             }
         });
+    }
 
+    // Show error message if there was an error that is being handled by.
+    public void errorMessage(String title, String message){
+        AlertDialog.Builder wrongInfo = new AlertDialog.Builder(SignupActivity.this);
+        wrongInfo.setCancelable(true);
+        wrongInfo.setTitle(title);
+        wrongInfo.setMessage(message);
+        wrongInfo.show();
+        etPassword.setText("");
+        etRePassword.setText("");
+        etEmail.setText("");
+        etName.setText("");
+    }
 
+    //Check if the string passed in is in an email format.
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return (email).matches(emailRegex);
     }
 }
