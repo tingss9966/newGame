d08a27d4b2f1ec210b3bae9b8a5fd7149f41fa29
Bei <jiayu.pang@mail.utoronto.ca>
2019-10-19 16:01:31 -0400

    Add getters and method attack for Tower.java

--- app/src/main/java/TowerDefense/Tower.java
+++ app/src/main/java/TowerDefense/Tower.java
@@ -1,10 +1,31 @@
 package TowerDefense;
 
-class Tower {
-    int cost;
-    int firingFrequency;
-    int range;
-    Ammunition ammo;
+abstract class Tower {
+    private int cost;
+    private int firingFrequency;
+    private int range;
+    private Ammunition ammo;
 
+    Tower(){
+    }
+
+    abstract Ammunition attack();
+
+
+    int getCost() {
+        return cost;
+    }
+
+    int getFiringFrequency() {
+        return firingFrequency;
+    }
+
+    int getRange() {
+        return range;
+    }
+
+    Ammunition getAmmo() {
+        return ammo;
+    }
 
 }
