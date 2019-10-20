75c1818b04963161f05802423c5dc94e76f7aaa2
Bei <jiayu.pang@mail.utoronto.ca>
2019-10-19 22:47:49 -0400

    Add methods into GameView and MainThread
    Signed-off-by: Bei <jiayu.pang@mail.utoronto.ca>

--- app/src/main/java/com/example/userinterface/GameView.java
+++ app/src/main/java/com/example/userinterface/GameView.java
@@ -5,16 +5,27 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private MainThread thread;
+
     public GameView(Context context) {
         super(context);
 
         getHolder().addCallback(this);
 
+        thread = new MainThread(getHolder(), this);
+        setFocusable(true);
+
+    }
+
+    public void update() {
+
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+        thread.setRunning(true);
+        thread.start();
     }
 
     @Override
@@ -24,6 +35,16 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
 
     }
 }
